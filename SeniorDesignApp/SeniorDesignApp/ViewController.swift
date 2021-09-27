dcdfd85b80119bfd807065dc79681b1af36cc743
gmyslow <57652449+gmyslow@users.noreply.github.com>
2021-09-27 14:43:35 -0400

    idk
    idk

---
+++ SeniorDesignApp/SeniorDesignApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SeniorDesignApp
+//
+//  Created by Griffin Myslow on 9/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func modeOne(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func modeTwo(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func modeThree(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func modeFour(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func connectDevice(_ sender: UIButton) {
+    }
+}
+
