c339b9b79d033ea8d84d1d3077744e48d4651e28
Mac <tmacshaughnessy@gmail.com>
2021-09-28 20:37:46 -0400

    PAWS-5: Add OFF and make whole line clickable

--- PAWS-iOS/PAWS/PAWS/Views/VisulizerView.swift
+++ PAWS-iOS/PAWS/PAWS/Views/VisulizerView.swift
@@ -9,29 +9,73 @@ import Foundation
 import SwiftUI
 
 struct VisulizerView: View {
-    let mode = ["OFF", "Mode 1", "Mode 2", "Mode 3", "Mode 4", "Mode 5"]
-    @State var selectedMode: String? = nil
+    struct SelectableItem: Identifiable {
+        let id = UUID().uuidString
+        var name: String
+    }
+    var i=0;
+
+    var modes = [SelectableItem(name: "OFF")] + Array(0...5).map{SelectableItem(name: "Mode \($0)")}
+
+    @State var selectedItem: SelectableItem?
+    
     
     var body: some View {
-        ZStack {
-            NavigationView {
-                Form {
-                    ForEach(mode, id: \.self) { item in
-                                    SelectionCell(mode: item, selectedMode: self.$selectedMode)
-                                }
+        NavigationView {
+            Form {
+                SingleSelectionList(items: modes, selectedItem: $selectedItem) { (item) in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                    }
                 }
-                .navigationBarTitle(Text("My Visuals"))
-                .navigationBarItems(leading:
-                                HStack {
-                                    Image("clemson-logo")
-                                        .resizable()
-                                        .foregroundColor(.white)
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 40, height: 40, alignment: .center)
-                                    .padding(UIScreen.main.bounds.size.width)
-                                }
-                        )
-            }.ignoresSafeArea()
+            }
+            .navigationBarTitle(Text("My Visuals"))
+            .navigationBarItems(leading:
+                            HStack {
+                                Image("clemson-logo")
+                                    .resizable()
+                                    .foregroundColor(.white)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40, alignment: .center)
+                                .padding(UIScreen.main.bounds.size.width)
+                            }
+                    )
+        }.ignoresSafeArea()
+    }
+}
+
+struct SingleSelectionList<Item: Identifiable, Content: View>: View {
+    var items: [Item]
+    @Binding var selectedItem: Item?
+    var rowContent: (Item) -> Content
+    
+    var body: some View {
+        List(items) { item in
+            rowContent(item)
+                .modifier(CheckmarkModifier(checked: item.id == self.selectedItem?.id))
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    self.selectedItem = item
+                }
+        }
+    }
+}
+
+struct CheckmarkModifier: ViewModifier {
+    var checked: Bool = false
+    func body(content: Content) -> some View {
+        Group {
+            if checked {
+                ZStack(alignment: .trailing) {
+                    content
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.accentColor)
+                        .shadow(radius: 1)
+                }
+            } else {
+                content
+            }
         }
     }
 }
@@ -49,8 +93,9 @@ struct SelectionCell: View {
                 Image(systemName: "checkmark")
                     .foregroundColor(.accentColor)
             }
-        }   .onTapGesture {
-                self.selectedMode = self.mode
-            }
+        }
+        .onTapGesture {
+            self.selectedMode = self.mode
+        }
     }
 }
