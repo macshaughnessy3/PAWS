7671028f8f06048d435b394f2138e7d9c9601d9e
Mac <tmacshaughnessy@gmail.com>
2021-10-05 21:31:25 -0400

    PAWS-9: Make MQTT always connect no button needed and send whole struct to pi

--- PAWS-iOS/PAWS/PAWS/Views/VisulizerView.swift
+++ PAWS-iOS/PAWS/PAWS/Views/VisulizerView.swift
@@ -10,50 +10,88 @@ import SwiftUI
 import CocoaMQTT
 let mqttClient = CocoaMQTT(clientID: "App", host: "172.22.37.4", port: 1883)
 
+struct SelectableItem: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+    let mode: Int
+}
 
 struct VisulizerView: View {
-    struct SelectableItem: Identifiable {
-        let id = UUID().uuidString
-        var name: String
+    var modes = [SelectableItem(name: "OFF", mode: -1)] + Array(0...5).map{SelectableItem(name: "Mode \($0)", mode: $0)}
+    @State var selectedItem: SelectableItem?
+    
+    init() {
+        _ = mqttClient.connect()
     }
     
     var body: some View {
-        NavigationView{
-            Form{
-            Button("Connect"){
-                print("yo")
-                mqttClient.connect()
-            }
-            Button("Mode 1")
-                {
-                    print("9dl")
-                    mqttClient.publish("raspberrypi/mode", withString: "mode1")
-                }
-            Button("Mode 2")
-                {
-                    print("9dl")
-                    mqttClient.publish("raspberrypi/mode", withString: "mode2")
-                }
-            Button("Mode 3")
-                {
-                    print("9dl")
-                    mqttClient.publish("raspberrypi/mode", withString: "mode3")
-                }
-            Button("Mode 4")
-                {
-                    print("9dl")
-                    mqttClient.publish("raspberrypi/mode", withString: "mode3")
-                }
-            Button("Mode 5")
-                {
-                    print("9dl")
-                    mqttClient.publish("raspberrypi/mode", withString: "mode4")
+        NavigationView {
+            Form {
+                SingleSelectionList(items: modes, selectedItem: $selectedItem) { item in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                    }
                 }
             }
             .navigationBarTitle(Text("My Visuals"))
             .navigationBarItems(leading:ClemsonLogoView())
+        }.ignoresSafeArea()
+    }
+}
 
+struct SingleSelectionList<Item: Identifiable, Content: View>: View {
+    var items: [Item]
+    @Binding var selectedItem: Item?
+    var rowContent: (Item) -> Content
+    var body: some View {
+        List(items) { item in
+            rowContent(item)
+                .modifier(CheckmarkModifier(checked: item.id == self.selectedItem?.id))
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    self.selectedItem = item
+                    mqttClient.publish("raspberrypi/mode", withString: "\(item)")
+                    print(item)
+                }
         }
     }
 }
 
+struct CheckmarkModifier: ViewModifier {
+    var checked: Bool = false
+    func body(content: Content) -> some View {
+        Group {
+            if checked {
+                ZStack(alignment: .trailing) {
+                    content
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.accentColor)
+                        .shadow(radius: 1)
+                }
+            } else {
+                content
+            }
+        }
+    }
+}
+
+//struct SelectionCell: View {
+//
+//    let mode: String
+//    @Binding var selectedMode: String?
+//
+//    var body: some View {
+//        HStack {
+//            Text(mode)
+//            Spacer()
+//            if mode == selectedMode {
+//                Image(systemName: "checkmark")
+//                    .foregroundColor(.accentColor)
+//            }
+//        }.onTapGesture {
+//            self.selectedMode = self.mode
+//            print("\(mode)")
+//        }
+//    }
+//}
