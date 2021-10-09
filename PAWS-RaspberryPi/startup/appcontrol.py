b0f1788dc673bf0e0ad99e6f46cda6ed4acb66dd
Mac Shaughnessy <tmacshaughnessy@gmail.com>
2021-10-08 21:35:17 -0400

    PAWS-11: print id

--- PAWS-RaspberryPi/startup/appcontrol.py
+++ PAWS-RaspberryPi/startup/appcontrol.py
@@ -4,9 +4,10 @@
 # mosquitto_pub -h raspberrypi.local -t "raspberrypi/move" -m "stop"
 
 import paho.mqtt.client as mqtt
+import json
 
 clientName = "pi"
-serverAddress = "raspberrypi"
+serverAddress = "macseansc-pi"
 mqttClient = mqtt.Client(clientName)
 
 def connectionStatus(client, userdata, flags, rc):
@@ -16,19 +17,39 @@ def connectionStatus(client, userdata, flags, rc):
 
 def messageDecoder(client, userdata, msg):
     message = msg.payload.decode(encoding='UTF-8')
-    
-    if message == "mode1":
-        print("Entered Mode 1!")
-    elif message == "mode2":
-        print("Entered Mode 2!")
-    elif message == "mode3":
-        print("Entered Mode 3!")
-    elif message == "mode4":
-        print("Entered Mode 4!")
-    elif message == "mode5":
-        print("Entered Mode 5!")
-    else:
-        print("?!? Unknown message?!?")
+    print(message)
+    # if message == "mode1":
+    #     print("Entered Mode 1!")
+    # elif message == "mode2":
+    #     print("Entered Mode 2!")
+    # elif message == "mode3":
+    #     print("Entered Mode 3!")
+    # elif message == "mode4":
+    #     print("Entered Mode 4!")
+    # elif message == "mode5":
+    #     print("Entered Mode 5!")
+    # elif message == "1":
+    #     print("Entered Mode 1!")
+    # elif message == "2":
+    #     print("Entered Mode 2!")
+    # elif message == "3":
+    #     print("Entered Mode 3!")
+    # elif message == "4":
+    #     print("Entered Mode 4!")
+    # elif message == "5":
+    #     print("Entered Mode 5!")
+    # elif message == "Mode 1":
+    #     print("Entered Mode 1!")
+    # elif message == "Mode 2":
+    #     print("Entered Mode 2!")
+    # elif message == "Mode 3":
+    #     print("Entered Mode 3!")
+    # elif message == "Mode 4":
+    #     print("Entered Mode 4!")
+    # elif message == "Mode 5":
+    #     print("Entered Mode 5!")
+    # else:
+    #     print("?!? Unknown message?!?")
 
 # Set up calling functions to mqttClient
 mqttClient.on_connect = connectionStatus
