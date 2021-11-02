9cd9c20c8f5fe2a60c04d2f0c61d59b9bfacdb6d
Mac <tmacshaughnessy@gmail.com>
2021-11-02 19:30:08 -0400

    PAWS-12: Add onboarding

--- PAWS-iOS/PAWS/PAWS/Views/ContentView.swift
+++ PAWS-iOS/PAWS/PAWS/Views/ContentView.swift
@@ -8,44 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @ObservedObject var viewModel: SongListViewModel
+    @State private var appSetupState = "App NOT setup ☹️"
+    @AppStorage("needsAppOnboarding") private var needsAppOnboarding: Bool = true
     
     var body: some View {
-        TabView {
-            SongView()
-                .tabItem {
-                Image(systemName: "music.note.list")
-                Text("Music")
-            }.tag(0)
-            VisulizerView()
-                .tabItem {
-                Image(systemName: "waveform")
-                Text("Visulizer")
-            }.tag(1)
-            HomeView(viewModel: viewModel)
-                .tabItem {
-                Image(systemName: "house.fill")
-                Text("Home")
-            }.tag(2)
-            SearchView(viewModel: viewModel)
-                .tabItem {
-                Image(systemName: "magnifyingglass")
-                Text("Search")
-            }.tag(3)
-            AccountView()
-                .tabItem {
-                Image(systemName: "person")
-                Text("Account")
-            }.tag(4)
+        mainView.onAppear {   
+            if !needsAppOnboarding {
+                // Scenario #3: User has completed app onboarding
+                appSetupState = "App setup 😀"
+            }
         }
-        .edgesIgnoringSafeArea(.top)
-        .onAppear() {
-            UITabBar.appearance().backgroundColor = .systemIndigo
-            UITabBar.appearance().unselectedItemTintColor = .systemGray3
-            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.systemIndigo]
+    }
+}
+
+extension ContentView {
+
+    private var mainView: some View {
+            VStack {
+                Button(action: {needsAppOnboarding = true}){}
+                    .sheet(isPresented:$needsAppOnboarding) {
+                        // Scenario #1: User has NOT completed app onboarding
+                        OnboardingView().environmentObject(CoreBluetoothViewModel())
+                    }
+                    .onChange(of: needsAppOnboarding) { needsAppOnboarding in
+
+                if !needsAppOnboarding {
+
+                // Scenario #2: User has completed app onboarding during current app launch
+                    appSetupState = "App setup 😀"
+                }
+            }
+            TabView {
+                SongView()
+                    .tabItem {
+                    Image(systemName: "music.note.list")
+                    Text("Music")
+                }.tag(0)
+                VisulizerView()
+                    .tabItem {
+                    Image(systemName: "waveform")
+                    Text("Visulizer")
+                }.tag(1)
+                HomeView(viewModel: viewModel)
+                    .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }.tag(2)
+                SearchView(viewModel: viewModel)
+                    .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }.tag(3)
+                AccountView()
+                    .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }.tag(4)
+            }
+            .edgesIgnoringSafeArea(.top)
+            .onAppear() {
+                UITabBar.appearance().backgroundColor = .systemIndigo
+                UITabBar.appearance().unselectedItemTintColor = .systemGray3
+                UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.systemIndigo]
+            }
+            .accentColor(.orange)
+
         }
-        .accentColor(.orange)
     }
 }
 
---
+++ PAWS-iOS/PAWS/PAWS/Views/OnboardingView.swift
@@ -0,0 +1,100 @@
+//
+//  OnboardingView.swift
+//  
+//
+//  Created by Mac Shaughnessy on 11/2/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct OnboardingView: View {
+    @EnvironmentObject var bleManager: CoreBluetoothViewModel
+
+    var body: some View {
+        
+        // #1
+            VStack {
+                Spacer(minLength: 150)
+                TabView {
+                    VStack {
+                        Text("Connect to the PAWS Speaker")
+                            .font(Font.title2.bold().lowercaseSmallCaps())
+                            .multilineTextAlignment(.center)
+                        List {
+                            ForEach(0..<bleManager.foundPeripherals.count, id: \.self) { num in
+                                if(bleManager.foundPeripherals[num].name != "NoName"){
+                                    Button(action: {
+                                        bleManager.connectPeripheral(bleManager.foundPeripherals[num])
+                                    }) {
+                                        HStack {
+                                            Text("\(bleManager.foundPeripherals[num].name)")
+                                            Spacer()
+                                            Text("\(bleManager.foundPeripherals[num].rssi) dBm")
+                                        }
+                                    }
+                                }
+                            }
+                        }.foregroundColor(Color.orange)
+                        .background(Color.gray)
+//                        .listRowBackground(Color.orange)
+                        VStack {
+                            Spacer()
+                            Button(action: {
+                                bleManager.isSearching ? bleManager.stopScan() : bleManager.startScan()
+                            }) {
+                                Text(bleManager.isSearching ? "Stop scanning" : "Start scanning")
+                                    .padding()
+                            }
+                        }
+                    }
+                    VStack {
+                        Image(systemName: "wand.and.stars")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80, alignment: .center)
+                        Text("Welcome To PAWS")
+                            .font(Font.title2.bold().lowercaseSmallCaps())
+                            .multilineTextAlignment(.center)
+                        Spacer(minLength: 60)
+                        Text("Go into bluetooth settings or use airplay to connect")
+                        Spacer(minLength: 30)
+                        Text("This app will control your visuals")
+                        Spacer(minLength: 30)
+                        Text("And finally 🥳...some...thing")
+                        
+                        // #2
+                        OnboardingButton()
+                    }
+                }
+                .tabViewStyle(.page)
+            }
+            .background(Color(.systemGray6))
+            .foregroundColor(Color(.systemIndigo))
+            .ignoresSafeArea(.all, edges: .all)
+    }
+}
+
+struct OnboardingButton: View {
+    
+    // #1
+    @AppStorage("needsAppOnboarding") var needsAppOnboarding: Bool = true
+    
+    var body: some View {
+        GeometryReader { proxy in
+            LazyHStack {
+                Button(action: {
+                    
+                    // #2
+                    needsAppOnboarding = false
+                }) {
+                    Text("Finish Setup")
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 15)
+                    .font(Font.title2.bold().lowercaseSmallCaps())
+                }
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height/1.5)
+        }
+    }
+}
