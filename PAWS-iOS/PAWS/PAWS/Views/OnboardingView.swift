d10556b502e3b24acc89886365da53be952f91fd
Mac <tmacshaughnessy@gmail.com>
2021-11-02 20:47:42 -0400

    quick

--- PAWS-iOS/PAWS/PAWS/Views/OnboardingView.swift
+++ PAWS-iOS/PAWS/PAWS/Views/OnboardingView.swift
@@ -1,105 +1,90 @@
 //
 //  OnboardingView.swift
-//  
+//  App Onboarding
 //
-//  Created by Mac Shaughnessy on 11/2/21.
+//  Created by Andreas Schultz on 10.08.19.
+//  Copyright © 2019 Andreas Schultz. All rights reserved.
 //
 
-import Foundation
+//Create your Onboarding view, tutorial can be found at: 
+//https://blckbirds.com/post/how-to-create-a-onboarding-screen-in-swiftui-1/
+
 import SwiftUI
 
 struct OnboardingView: View {
-    @EnvironmentObject var bleManager: CoreBluetoothViewModel
-    @StateObject var spotify = Spotify()
-
+    
+    var subviews = [
+        UIHostingController(rootView: Subview(imageString: "meditating")),
+        UIHostingController(rootView: Subview(imageString: "skydiving")),
+        UIHostingController(rootView: Subview(imageString: "sitting"))
+    ]
+    
+    var titles = ["Take some time out", "Conquer personal hindrances", "Create a peaceful mind"]
+    
+    var captions =  ["Take your time out and bring awareness into your everyday life", "Meditating helps you dealing with anxiety and other psychic problems", "Regular medidation sessions creates a peaceful inner mind"]
+    
+    @EnvironmentObject var viewRouter: ViewRouter
+    
+    @State var currentPageIndex = 0
+    
     var body: some View {
-        
-        // #1
-            VStack {
-                Spacer(minLength: 150)
-                TabView {
-                    VStack {
-                        Text("Connect to spotify")
-//                        RootView().environmentObject(spotify)
-                    }
-                    VStack {
-                        Text("Connect to the PAWS Speaker")
-                            .font(Font.title2.bold().lowercaseSmallCaps())
-                            .multilineTextAlignment(.center)
-                        List {
-                            ForEach(0..<bleManager.foundPeripherals.count, id: \.self) { num in
-                                if(bleManager.foundPeripherals[num].name != "NoName"){
-                                    Button(action: {
-                                        bleManager.connectPeripheral(bleManager.foundPeripherals[num])
-                                    }) {
-                                        HStack {
-                                            Text("\(bleManager.foundPeripherals[num].name)")
-                                            Spacer()
-                                            Text("\(bleManager.foundPeripherals[num].rssi) dBm")
-                                        }
-                                    }
-                                }
-                            }
-                        }.foregroundColor(Color.orange)
-                        .background(Color.gray)
-//                        .listRowBackground(Color.orange)
-                        VStack {
-                            Spacer()
-                            Button(action: {
-                                bleManager.isSearching ? bleManager.stopScan() : bleManager.startScan()
-                            }) {
-                                Text(bleManager.isSearching ? "Stop scanning" : "Start scanning")
-                                    .padding()
-                            }
+        VStack(alignment: .leading) {
+            PageViewController(currentPageIndex: $currentPageIndex, viewControllers: subviews)
+                .frame(height: 600)
+            Group {
+                Text(titles[currentPageIndex])
+                    .font(.title)
+                Text(captions[currentPageIndex])
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .frame(width: 300, height: 50, alignment: .leading)
+                .lineLimit(nil)
+            }
+                .padding()
+            HStack {
+                PageControl(numberOfPages: subviews.count, currentPageIndex: $currentPageIndex)
+                Spacer()
+                if currentPageIndex == 2 {
+                    Button(action: {
+                        withAnimation {
+                            self.viewRouter.currentPage = "homeView"
                         }
+                    }) {
+                        ButtonContent()
                     }
-                    VStack {
-                        Image(systemName: "wand.and.stars")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80, alignment: .center)
-                        Text("Welcome To PAWS")
-                            .font(Font.title2.bold().lowercaseSmallCaps())
-                            .multilineTextAlignment(.center)
-                        Spacer(minLength: 60)
-                        Text("Go into bluetooth settings or use airplay to connect")
-                        Spacer(minLength: 30)
-                        Text("This app will control your visuals")
-                        Spacer(minLength: 30)
-                        Text("And finally 🥳...some...thing")
-                        
-                        // #2
-                        OnboardingButton()
+                } else {
+                    Button(action: {
+                        if self.currentPageIndex+1 == self.subviews.count {
+                            self.currentPageIndex = 0
+                        } else {
+                            self.currentPageIndex += 1
+                        }
+                    }) {
+                        ButtonContent()
                     }
                 }
-                .tabViewStyle(.page)
             }
-            .background(Color(.systemGray6))
-            .foregroundColor(Color(.systemIndigo))
-            .ignoresSafeArea(.all, edges: .all)
-    }
-}
-
-struct OnboardingButton: View {
-    
-    // #1
-    @AppStorage("needsAppOnboarding") var needsAppOnboarding: Bool = true
-    
-    var body: some View {
-        GeometryReader { proxy in
-            LazyHStack {
-                Button(action: {
-                    
-                    // #2
-                    needsAppOnboarding = false
-                }) {
-                    Text("Finish Setup")
-                    .padding(.horizontal, 40)
-                    .padding(.vertical, 15)
-                    .font(Font.title2.bold().lowercaseSmallCaps())
-                }
-            }
-            .frame(width: proxy.size.width, height: proxy.size.height/1.5)
+                .padding()
         }
     }
 }
+
+struct ButtonContent: View {
+    var body: some View {
+        Image(systemName: "arrow.right")
+            .resizable()
+            .foregroundColor(.white)
+            .frame(width: 30, height: 30)
+            .padding()
+            .background(Color.orange)
+            .cornerRadius(30)
+    }
+}
+
+#if DEBUG
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView().environmentObject(ViewRouter())
+    }
+}
+#endif
