24ea968060b6f1930f070f301c342c03ce2bdcc1
Mac <tmacshaughnessy@gmail.com>
2021-11-15 00:01:10 -0500

    Final version 4.0: paw tap

--- PAWS-iOS/PAWS/PAWS/Views/ClemsonLogoView.swift
+++ PAWS-iOS/PAWS/PAWS/Views/ClemsonLogoView.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 import SwiftUI
+import CoreBluetooth
+import CoreData
+
 
 struct ClemsonLogoView: View {
-//    var padding : Int
+    @EnvironmentObject var bleManager: CoreBluetoothViewModel
+
     var body: some View {
         HStack {
             Image("clemson-logo")
@@ -17,7 +21,13 @@ struct ClemsonLogoView: View {
                 .foregroundColor(.white)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40, alignment: .center)
-//            .padding(UIScreen.main.bounds.size.width)
+                .onTapGesture {
+                    if self.bleManager.isConnected {
+                        self.bleManager.connectedPeripheral.peripheral.writeValue(("255_255_255_5_GoTigers" as NSString).data(using: String.Encoding.utf8.rawValue)!, for:  bleManager.foundCharacteristics.first(where: { Characteristic in
+                            return Characteristic.uuid.isEqual(CBUUID(string: "6e400002-b5a3-f393-e0a9-e50e24dcca9e"))
+                        })!.characteristic, type: CBCharacteristicWriteType.withResponse)
+                }
+            }
         }
     }
 }
